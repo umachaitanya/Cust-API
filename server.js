7c9c53a68e1f3688c00ff2e60ef5328a2429508c
umachaitanya <umachaitanya3@gmail.com>
2020-04-27 03:10:23 -0400

    changed

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/CustDB', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
+app.use(express.json())
+
+const customersRouter = require('./routes/customers')
+app.use('/customers', customersRouter)
+
+app.listen(3000, () => console.log('server started'))
